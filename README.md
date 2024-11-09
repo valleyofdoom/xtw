dd2c3d18c96f1bb18f83b6c18714f53e47b59f86
valleyofdoom <173058957+valleyofdoom@users.noreply.github.com>
2024-11-09 16:07:08 +0000

    Add build workflow and README

---
+++ README.md
@@ -0,0 +1,11 @@
+# XTW
+
+[![Downloads](https://img.shields.io/github/downloads/valleyofdoom/xtw/total.svg)](https://github.com/valleyofdoom/xtw/releases)
+
+## Usage
+
+```
+xtw.exe --etl_file <path to etl>
+```
+
+Example: ``xtw.exe --etl_file C:\kernel.etl``
---
+++ build.ps1
@@ -0,0 +1,32 @@
+function Is-Admin() {
+    $currentPrincipal = New-Object Security.Principal.WindowsPrincipal([Security.Principal.WindowsIdentity]::GetCurrent())
+    return $currentPrincipal.IsInRole([Security.Principal.WindowsBuiltInRole]::Administrator)
+}
+
+function main() {
+    if (-not (Is-Admin)) {
+        Write-Host "error: administrator privileges required"
+        return 1
+    }
+
+    if (Test-Path ".\build\") {
+        Remove-Item -Path ".\build\" -Recurse -Force
+    }
+
+    mkdir ".\build\"
+
+    # create folder structure
+    mkdir ".\build\xtw\"
+
+    # build application
+    MSBuild.exe ".\xtw.sln" -p:Configuration=Release -p:Platform=x64
+
+    # create final package
+    Copy-Item ".\xtw\bin\x64\Release\*" ".\build\xtw\"
+
+    return 0
+}
+
+$_exitCode = main
+Write-Host # new line
+exit $_exitCode
