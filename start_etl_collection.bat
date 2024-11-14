78edceb80b5f0b8c5a0f62c55f8bfb20ec831a28
valleyofdoom <173058957+valleyofdoom@users.noreply.github.com>
2024-11-14 12:13:04 +0000

    Update script

--- start_etl_collection.bat
+++ start_etl_collection.bat
@@ -1,6 +1,14 @@
 @echo off
+pushd %~dp0
 
-xtw --delay 3 --timed 10
+xtw.exe --delay 3 --timed 10
+
+if not %errorlevel%  == 0 (
+    pause
+    exit /b 1
+)
+
+start notepad xtw-report.txt
 
 pause
 exit /b 0
